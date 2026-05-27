bd228f3f417f921c0ecaeb139551fe5502bd987b
bernat-corommina <bernat.coromina@glovoapp.com>
2026-05-27 17:07:28 +0200

    type classes

--- src/main/scala/10- TypeClasses.scala
+++ src/main/scala/10- TypeClasses.scala
@@ -26,6 +26,7 @@ object JsonSerializableInstances:
 //        case Some(value) => s.toJsonT(value)
 //        case None        => "{}" // or null. null is a valid json value
 
+    // Composition: If I know how to serialize T, I can serialize Option[T] without knowing the details of T.
     given optionJsonSerializable[T: JsonSerializable]: JsonSerializable[Option[T]] with
       def toJsonT(v: Option[T]): String = v match
         case Some(value) => summon[JsonSerializable[T]].toJsonT(value)
@@ -37,9 +38,17 @@ object JsonSerializableInstances:
 //      def toJsonD: String =
 //        summon[JsonSerializable[T]].toJson(value)
 
-def toJson[T: JsonSerializable](value: T): String =
-  summon[JsonSerializable[T]].toJsonT(value)
+def toJson[T](value: T)(using js: JsonSerializable[T]): String =
+  js.toJsonT(value)
 
+//def toJson[T: JsonSerializable](value: T): String =
+//  summon[JsonSerializable[T]].toJsonT(value)
+
+// the required type classes are the properties of the type.
+def printList[T: JsonSerializable](l: List[T]): Unit =
+  l.foreach { item =>
+    println(s"Object: ${item.toJsonExt}")
+  }
 
 extension [T: JsonSerializable](value: T)
   def toJsonExt: String =
@@ -48,6 +57,16 @@ extension [T: JsonSerializable](value: T)
 
 @main def jsonSerializer(): Unit =
   import JsonSerializableInstances.given
+
+  //ad-hoc polymorphism: We define specific behavior for specific types.
+  //vs subtype polymorphism: We define behavior for a type and all its subtypes.
+  //vs parametric polymorphism: We define behavior for all types. //def identity[T](x: T): T = x
+
+
+  //NOTE: function overloading is also ad-hoc polymorphism.
+  //Function print does different things for each type String or Int:
+  // def print(x: Int)
+  // def print(x: String)
 
   val user = User("Bernat", 44)
 
