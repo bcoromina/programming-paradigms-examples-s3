a8051177f4afa476d421bf3debf9d090f165de06
bernat-corommina <bernat.coromina@glovoapp.com>
2026-05-27 16:50:30 +0200

    completed

--- src/main/scala/10- TypeClasses.scala
+++ src/main/scala/10- TypeClasses.scala
@@ -1,49 +1,58 @@
 // type class pattern
 // is used to add new functionality to existing types without modifying their code.
 trait JsonSerializable[T]:
-  def toJson(v: T): String
+  def toJsonT(v: T): String
 
 case class User(name: String, age: Int)
 case class House(address: String, price: BigDecimal)
 
-object JsonSerializableInstances
+object JsonSerializableInstances:
 
-  given JsonSerializable[User] with
-    def toJson(p: User): String =
-      s"""{"name": "${p.name}", "age": ${p.age}}"""
+//  given JsonSerializable[User] with
+//    def toJson(p: User): String =
+//      s"""{"name": "${p.name}", "age": ${p.age}}"""
 
-//  given userSerializer: JsonSerializable[User] =
-//    new JsonSerializable[User]:
-//      def toJson(p: User): String =
-//        s"""{"name": "${p.name}", "age": ${p.age}}"""
+    given userSerializer: JsonSerializable[User] =
+      new JsonSerializable[User]:
+        def toJsonT(p: User): String =
+          s"""{"name": "${p.name}", "age": ${p.age}}"""
 
-  given JsonSerializable[House] with
-    def toJson(p: House): String =
-      s"""{"address": "${p.address}", "price": ${p.price}}"""
+    given JsonSerializable[House] with
+      def toJsonT(p: House): String =
+        s"""{"address": "${p.address}", "price": ${p.price}}"""
 
-  given optionSerializer[T](using s: JsonSerializable[T]): JsonSerializable[Option[T]] with
-    def toJson(v: Option[T]): String = v match
-      case Some(value) => s.toJson(value)
-      case None        => "{}" // or null. null is a valid json value
+//    given optionSerializer[T](using s: JsonSerializable[T]): JsonSerializable[Option[T]] with
+//      def toJsonT(v: Option[T]): String = v match
+//        case Some(value) => s.toJsonT(value)
+//        case None        => "{}" // or null. null is a valid json value
 
-//  given [T: JsonSerializable]: JsonSerializable[Option[T]] with
-//    def toJson(v: Option[T]): String = v match
-//      case Some(value) => value.toJson
-//      case None => "{}"
+    given optionJsonSerializable[T: JsonSerializable]: JsonSerializable[Option[T]] with
+      def toJsonT(v: Option[T]): String = v match
+        case Some(value) => summon[JsonSerializable[T]].toJsonT(value)
+        case None        => "{}" // or null. null is a valid json value
 
-def toJsonT[T: JsonSerializable](value: T): String =
-  summon[JsonSerializable[T]].toJson(value)
+
+
+//    extension [T: JsonSerializable](value: T)
+//      def toJsonD: String =
+//        summon[JsonSerializable[T]].toJson(value)
+
+def toJson[T: JsonSerializable](value: T): String =
+  summon[JsonSerializable[T]].toJsonT(value)
+
 
 extension [T: JsonSerializable](value: T)
-  def toJson: String =
-    summon[JsonSerializable[T]].toJson(value)
+  def toJsonExt: String =
+    summon[JsonSerializable[T]].toJsonT(value)
 
 
 @main def jsonSerializer(): Unit =
-  import JsonSerializableInstances._
+  import JsonSerializableInstances.given
+
   val user = User("Bernat", 44)
-  println(user.toJson)
 
-  println(Option(user).toJson)
+  println(user.toJsonExt)
 
-  println(toJsonT(user))
+  println(Option(user).toJsonExt)
+
+  println(toJson(user))
